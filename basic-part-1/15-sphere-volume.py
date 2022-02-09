25d78c8b385cf3c1344a3350b9629d3d8136ce43
Inderpal Singh <inderpal2406@gmail.com>
2022-02-10 00:16:17 +0530

    Add 15-sphere-volume.py.

---
+++ basic-part-1/15-sphere-volume.py
@@ -0,0 +1,17 @@
+# This script will calculate volume of a speher with radius 6.
+
+# Import modules.
+
+import math
+
+# Initialize variables.
+
+radius = 6
+
+# Calculate volume of sphere.
+
+volume = (4*math.pi*radius**3)/3
+
+# Print volume.
+
+print(f"\nThe volume of the sphere with radius 6 is {volume}.\n")
