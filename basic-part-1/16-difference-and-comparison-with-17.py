7034db3d3475a0ffb5ebd09976ca430c29831f08
Inderpal Singh <inderpal2406@gmail.com>
2022-02-10 00:44:13 +0530

    Add 16-difference-and-comparison-with-17.py.

---
+++ basic-part-1/16-difference-and-comparison-with-17.py
@@ -0,0 +1,28 @@
+# This script will get the difference between a given number and 17.
+# If the given number is greater than 17, then double of the difference is returned as answer.
+
+# Import modules.
+
+import clear_screen_module
+
+# Clear the screen.
+
+clear_screen_module.clear_screen()
+
+# Display purpose of the script.
+
+print(f"This script will accept a number and display its difference with 17.")
+print(f"If the provided number is greater than 17, then double the difference would be returned.\n")
+
+# Accept the number from the user.
+
+num = eval(input("Enter the number: "))
+
+# Calculate the difference between the number and 17.
+
+if num > 17:
+    double_difference = 2*(num-17)
+    print(f"\nAs the number {num} is greater than 17, the double of the difference is {double_difference}.\n")
+else:
+    difference = 17-num
+    print(f"\nDifference between 17 and the {num} is {difference}.\n")
