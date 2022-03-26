e37aa8db55cc19ee8c4a8f9ab72c8ae971625874
Inderpal Singh <inderpal2406@gmail.com>
2022-03-26 16:11:17 +0530

    Update 48-parse-str-to-float-int.py to fix pylint errors.

--- basic-part-1/48-parse-str-to-float-int.py
+++ basic-part-1/48-parse-str-to-float-int.py
@@ -8,24 +8,27 @@ import time
 # Define functions.
 
 def main():
+    """Main function to do everything"""
     # Clear the screen.
     clear_screen_module.clear_screen()
     # Display purpose of the script.
-    print(f"This script has a pre-defined string which will be converted to float and integer.\n")
+    print("This script has a pre-defined string which will be converted to float and integer.\n")
     predef_str = "2.486"
     print(f"The pre-defined string is: {predef_str}\nIt's data type: {type(predef_str)}\n")
     time.sleep(1)
-    print(f"Converting pre-defined string to float data type...")
+    print("Converting pre-defined string to float data type...")
     time.sleep(2)
     float_data = float(predef_str)
-    print(f"The value after conversion to float is: {float_data}\nNew data type: {type(float_data)}\n")
+    print(f"The value after conversion to float is: {float_data}")
+    print(f"New data type: {type(float_data)}\n")
     time.sleep(1)
-    print(f"Converting float data to integer data type...")
+    print("Converting float data to integer data type...")
     time.sleep(2)
     int_data = int(float_data)
-    print(f"The value after conversion to integer is: {int_data}\nNew data type: {type(int_data)}\n")
+    print(f"The value after conversion to integer is: {int_data}")
+    print(f"New data type: {type(int_data)}\n")
     time.sleep(1)
-    print(f"Completed the type casting of data. That's it for now. Be happy :)")
+    print("Completed the type casting of data. That's it for now. Be happy :)")
     return None
 
 # Main code starts here.
