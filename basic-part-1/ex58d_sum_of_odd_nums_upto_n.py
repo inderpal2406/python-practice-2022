c0c4298d051470a28998d189fc11ade7b5450ce6
Inderpal Singh <inderpal2406@gmail.com>
2022-03-27 20:26:43 +0530

    Add ex58d_sum_of_odd_nums_upto_n.py.

---
+++ basic-part-1/ex58d_sum_of_odd_nums_upto_n.py
@@ -0,0 +1,44 @@
+"""Calculate sum of odd numbers upto number n"""
+
+# This script will calculate sum of odd numbers upto number n.
+# number n would be accepted from the user.
+
+# Import modules.
+
+import sys
+import clear_screen_module
+
+# Define functions.
+
+def sum_of_odd_nums_upto_n(i):
+    """Sum of odd nums upto n"""
+    num = 1
+    ans = 0
+    while num <= i:
+        if num % 2 == 1:
+            ans = ans + num
+        num = num + 1
+    return ans
+
+def main():
+    """First function to be called"""
+    # Clear screen using module function.
+    clear_screen_module.clear_screen()
+    # Display purpose of the script.
+    print("This script will calculate sum of odd numbers upto number n.\n")
+    # Accept integer n using try-except block.
+    try:
+        n = int(input("Enter n: "))
+    except ValueError:
+        print("This script accepts only integer value for n. Exiting script!")
+        sys.exit(1)
+    n = abs(n)  # Covert negative num to positive if user provides negative int.
+    # Call function to calculate sum.
+    our_sum = sum_of_odd_nums_upto_n(n)
+    print(f"\nSum of odd numbers upto {n} is: {our_sum}\n")
+    return None
+
+# Call main function if this script is executed.
+
+if __name__ == "__main__":
+    main()
