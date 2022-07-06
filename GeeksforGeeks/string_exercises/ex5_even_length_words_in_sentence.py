b37c11b8bc2bbd7aa99871b03e05cef2cd290fbc
Inderpal Singh <inderpal2406@gmail.com>
2022-07-06 23:41:26 +0530

    Update var names and logic in script to find even length words in sentence.

--- GeeksforGeeks/string_exercises/ex5_even_length_words_in_sentence.py
+++ GeeksforGeeks/string_exercises/ex5_even_length_words_in_sentence.py
@@ -6,25 +6,25 @@ import clear_screen_module
 
 # Define functions.
 
-def FindEvenLenWords(wordlist):
+def FindEvenLenWords(ourstr):
     """Function to find even length words in a list"""
+    wordlist = ourstr.split()
     anslist = []
-    for eachstr in wordlist:
-        if len(eachstr)%2 == 0:
-            anslist.append(eachstr)
+    for eachword in wordlist:
+        if len(eachword)%2 == 0:
+            anslist.append(eachword)
     return anslist
 
 def main():
     """First function to be called"""
     clear_screen_module.clear_screen()
-    print("This script finds even length words in a string.\n")
-    userstr = input("Enter the string: ")
-    strlist = userstr.split()
-    evenlenstr = FindEvenLenWords(strlist)
-    if len(evenlenstr) == 0:
+    print("This script finds even length words in a sentence.\n")
+    userstr = input("Enter the sentence: ")
+    EvenLenWordsList = FindEvenLenWords(userstr)
+    if len(EvenLenWordsList) == 0:
         print("No even length words found.\n")
     else:
-        print(f"Even length words are: {evenlenstr}\n")
+        print(f"Even length words are: {EvenLenWordsList}\n")
 
 if __name__ == "__main__":
     main()
