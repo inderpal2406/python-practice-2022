1cd7db5d3621b536cde1d151d2911f2c820af762
Inderpal Singh <inderpal2406@gmail.com>
2022-09-15 19:01:29 +0530

    Add script to remove special symbols from a str.

---
+++ PYnative/04-string-exercises/ex15_remove_special_symbols_from_str.py
@@ -0,0 +1,6 @@
+# We are skipping exercise 15 from below website,
+# https://pynative.com/python-string-exercise/#h-exercise-12-find-the-last-position-of-a-given-substring
+# As per my view, the soluton can be implemented using ASCII but would not be efficient.
+# An efficient solution using str functions translate(), maketrans() is suggested on the website,
+# but needs more understanding. Also regex solution is given on the website. That also needs
+# more understanding.
